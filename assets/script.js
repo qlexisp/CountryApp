6f2d3d5d602787cfff56b59384ed42f310cec95a
alex <1alexispetropoulos@gmail.com>
2024-04-02 11:40:47 +0200

    ajout toggle menu

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById("menu");
+let onClick = document.getElementById("onClick");
+
+onClick.addEventListener('click', function() {
+    menu.classList.toggle('hidden');
+});
