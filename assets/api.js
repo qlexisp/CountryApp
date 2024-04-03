1598da0509a1492cd33cab366285b419226d9316
alex <1alexispetropoulos@gmail.com>
2024-04-03 11:15:19 +0200

    country cards

--- assets/api.js
+++ assets/api.js
@@ -1,8 +1,17 @@
+let showCountriesInfos;
+
 document.getElementById('websiteSearch').addEventListener('keyup', e => { // addEventListener de la search bar
     if (e.code === "Enter") {
         let country = document.getElementById('websiteSearch').value;
         showCountries(country);
         getCountries(country);
+    }
+});
+
+document.getElementById('showCountries').addEventListener('click', c => { // addEventListener click on flag
+    if (c) {
+        deleteContent();
+        countryCard();
     }
 });
 
@@ -14,8 +23,8 @@ async function getCountries(country) { // Récupération des informations
 
         if (response) {
             const countryInfos = response[0];
-            console.log(countryInfos.name.nativeName);
-            return {
+            console.log(countryInfos.borders);
+            showCountriesInfos = {
                 flag: countryInfos.flags.svg,
                 name: countryInfos.name.common,
                 nativeName: countryInfos.name.nativeName, // pas sûr
@@ -28,6 +37,7 @@ async function getCountries(country) { // Récupération des informations
                 languages: countryInfos.languages,
                 borders: countryInfos.borders // pas sûr
             };
+            return showCountriesInfos;
         }
         else {
             console.error('Aucune information trouvée pour la ville spécifiée.');
@@ -45,13 +55,38 @@ async function showCountries(country) { // Affichage des données récupérées
     if (showCountriesInfos) {
         document.getElementById('showCountries').innerHTML = `
         <div class="bg-white my-8 w-[80%] shadow-md rounded-md">
-        <img class="mb-6 rounded-t-lg" src="${showCountriesInfos.flag}">
-        <div class="pl-6 pb-12">
-        <p class="font-bold text-lg mb-4">${showCountriesInfos.name}</p>
-        <p class="text-sm"><span class="font-semibold">Population:</span> ${showCountriesInfos.population}</p>
-        <p class="text-sm"><span class="font-semibold">Region:</span> ${showCountriesInfos.region}</p>
-        <p class="text-sm"><span class="font-semibold">Capital:</span> ${showCountriesInfos.capital}</p>
-        </div>
+            <img class="mb-6 rounded-t-lg" src="${showCountriesInfos.flag}">
+            <div class="pl-6 pb-12">
+                <p class="font-bold text-lg mb-4">${showCountriesInfos.name}</p>
+                <p class="text-sm"><span class="font-semibold">Population:</span> ${showCountriesInfos.population}</p>
+                <p class="text-sm"><span class="font-semibold">Region:</span> ${showCountriesInfos.region}</p>
+                <p class="text-sm"><span class="font-semibold">Capital:</span> ${showCountriesInfos.capital}</p>
+            </div>
         </div>`;
     }
+}
+
+function deleteContent() { // Supprime le contenu de la div "showCountries" en cas d'event
+    document.getElementById('showCountries').innerHTML = ``;
+};
+
+function countryCard() { // Contenu 
+    document.getElementById('showCountries').innerHTML = `
+    <div class"my-20 bg-white shadow-md">
+        <div class="">
+            <img class="" src="assets/img/arrow_back.png">
+            <p class="font-bold">Back</p>
+        </div>
+
+        <img class="" src="${showCountriesInfos.flag}">
+        <div class="">
+            <p class="">${showCountriesInfos.name}</p>
+            <p class="">${showCountriesInfos.name.nativeName}</p>
+            <p class="">${showCountriesInfos.population}</p>
+            <p class="">${showCountriesInfos.region}</p>
+            <p class="">${showCountriesInfos.subregion}</p>
+            <p class="">${showCountriesInfos.capital}</p>
+        </div>
+    </div>
+    `;
 }
